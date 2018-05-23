acf6f817634d42b8598e03830b1ebbcec22afdb1
Poimulaz <poimulaz@gmail.com>
2018-05-23 10:51:44 +0200

    echo array

--- Controllers/Control.php
+++ Controllers/Control.php
@@ -15,20 +15,29 @@ class Control
 
         echo '
 <hr />La dernière ligne en sortie de la commande : ' . $last_line . '
-<hr />Valeur retournée : ' . $retval;
+<hr />Valeur retournée : ';
+        foreach ($retval as $line) {
+            echo "$line\n";
+        }
 
-        $last_line = exec('/etc/init.d/apache2 status', $retval);
-
+        $last_line = exec('/etc/init.d/apache2 status | grep active', $retval);
+/*
         $retval = explode("Active: ", $retval)[1];
-        $retval = substr($retval, 0,6);
-        echo '<div>retval =' . $retval . '</div>';
+        $retval = substr($retval, 0,6);*/
+        foreach ($retval as $line) {
+            echo "$line\n";
+        }
         /*if($retval){
             $ip = false;
         }
         else $ip =true;*/
         echo '
 <hr />La dernière ligne en sortie de la commande : ' . $last_line . '
-<hr />Valeur retournée : ' . $retval;
+<hr />Valeur retournée : ';
+
+        foreach ($retval as $line) {
+            echo "$line\n";
+        }
 
 
         require 'Views/dashboard.php';
