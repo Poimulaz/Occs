d892ae3fc085ee2a92dfc4c1e6714cdd7320f899
Poimulaz <poimulaz@gmail.com>
2018-05-23 10:11:28 +0200

    test status + mem

--- Controllers/Control.php
+++ Controllers/Control.php
@@ -10,32 +10,24 @@ class Control
 {
     public static function home()
     {
-        // var_dump($_SESSION['user']); // ici l'objet est perdu -> object(__PHP_Incomplete_Class) (3)
-        //shell_exec("echo 1");
-        echo exec("text.bat");
-        //echo exec("C:/Users/Pierre/Desktop/Ocw_script.bat");
 
-        $last_line = system('Ocw_script.bat', $retval);
+        $last_line = system('free -m', $retval);
 
-        // Affichage d'autres informations
         echo '
 <hr />La dernière ligne en sortie de la commande : ' . $last_line . '
 <hr />Valeur retournée : ' . $retval;
 
-       // $last_line = system('ping 8.8.8.8', $retval);
+        $last_line = system('/etc/init.d/apache2 status', $retval);
 
-        if($retval){
+        /*if($retval){
             $ip = false;
         }
-        else $ip =true;
-
-        // Affichage d'autres informations
+        else $ip =true;*/
         echo '
 <hr />La dernière ligne en sortie de la commande : ' . $last_line . '
 <hr />Valeur retournée : ' . $retval;
 
 
         require 'Views/dashboard.php';
-        //shell_exec("dir");
     }
 }
