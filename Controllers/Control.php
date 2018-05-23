bce14e2209f008c1c2e40b9b724e6b7afab13be2
Poimulaz <poimulaz@gmail.com>
2018-05-23 10:44:54 +0200

    system to exec

--- Controllers/Control.php
+++ Controllers/Control.php
@@ -11,13 +11,13 @@ class Control
     public static function home()
     {
 
-        $last_line = system('free -m', $retval);
+        $last_line = exec('free -m', $retval);
 
         echo '
 <hr />La dernière ligne en sortie de la commande : ' . $last_line . '
 <hr />Valeur retournée : ' . $retval;
 
-        $last_line = system('/etc/init.d/apache2 status', $retval);
+        $last_line = exec('/etc/init.d/apache2 status', $retval);
 
         $retval = explode("Active: ", $retval)[1];
         $retval = substr($retval, 0,6);
